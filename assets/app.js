4ccc7cb3975048b275a9e6e7e3765b1279b46b0f
Baz <jennigoldstein@gmail.com>
2018-08-09 16:12:52 -0500

    update

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function () {
 
     var config = {
@@ -5,66 +6,71 @@ $(document).ready(function () {
         authDomain: "train-time-ecca6.firebaseapp.com",
         databaseURL: "https://train-time-ecca6.firebaseio.com",
         projectId: "train-time-ecca6",
-        storageBucket: "",
+        storageBucket: "train-time-ecca6.appspot.com",
         messagingSenderId: "912251801862"
-      };
-      firebase.initializeApp(config);
-   
-    
-      var database= firebase.database();
+    };
+    firebase.initializeApp(config);
 
-$("#sButton").on("click", function(){
-        var name = $("#name-input").val();
-        var destination = $("#destination-input").val();
-       var frequency = $("#frequency-input").val();
-       var firstTime = moment(($("#time-input").val()), "HH:mm").subtract(1, "years");
-       var currentTime = moment();
-       var diffTime = moment().add(moment(firstTime), "minutes");
-       var remainder = diffTime % frequnncy; 
-       var minutesAway= frequency - remainder; 
-        var nextArrival = (moment().add(minutesAway,'minutes'));
-        
-        
-    
 
-    database.ref().push({
-        name: name,
-        destination: destination,
-        frequency: frequency,
-        firstTime: firstTime,
-        dateAdded:firebase.database.ServerValue.TIMESTAMP
-    });
+    var database = firebase.database();
 
-    database.ref().on("child_added", function(childSnapshot){
-       
-        var newRow=$("<tr>");
+
+
+    database.ref().on("child_added", function (childSnapshot) {
+        var frequency = parseInt(childSnapshot.val().frequency);
+        var firstTime = childSnapshot.val().firstTime;
+        var currentTime = moment();
+        var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+        var diffTime = currentTime.diff(moment(firstTimeConverted), "minutes");
+        var remainder = diffTime % frequency;
+        var minutesAway = frequency - remainder;
+        var nextArrival = moment(currentTime).add(minutesAway, "minutes");
+        var nextTrainFormatted = moment(nextArrival).format("HH:mm");
         
+        var newRow = $("<tr>");
 
         newRow.html(
             "<td>" + childSnapshot.val().name +
             "<td>" + childSnapshot.val().destination +
-            "<td>" + childSnapshot.val().frequency +
-            "<td>" + nextArrival +
+            "<td>" + frequency +
+            "<td>" + nextTrainFormatted +
             "<td>" + minutesAway
         );
 
         $("tbody").append($(newRow));
 
-        
+    });
 
-   
-         
+    $("#sButton").on("click", function (event) {
+        event.preventDefault();
+        var name = $("#name-input").val().trim();
+        var destination = $("#destination-input").val().trim();
+        var frequency = $("#frequency-input").val().trim();
+        var firstTime = $("#time-input").val().trim();
 
+
+        database.ref().push({
+            name: name,
+            destination: destination,
+            frequency: frequency,
+            firstTime: firstTime,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+
+        $("#name-input").val("");
+        $("#destination-input").val("");
+        $("#frequency-input").val("");;
+        $("#time-input").val("");
     });
 
 
 
-});
 
 
 
-  
 
 
-  
+
+
+
 });
